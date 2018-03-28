54f8ab5800e9a124e3ff22c472816cb1b8ccde58
Deimon <blanco.damian@gmail.com>
2018-03-28 16:26:22 -0300

    Template JUnit5

--- src/com/deimon/isfpp/junit/CiudadTest.java
+++ src/com/deimon/isfpp/junit/CiudadTest.java
@@ -2,13 +2,45 @@ package com.deimon.isfpp.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class CiudadTest {
 
-	@Test
-	void myFirstTest() {
+	@BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    @Test
+    void succeedingTest() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void failingTest() {
+        fail("a failing test");
+    }
+
+    @Test
+    @Disabled("for demonstration purposes")
+    void skippedTest() {
+        // not executed
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
     }
 
 }
