19b2b4284dd4e70d72540a29df5dd7310da13c21
Deimon <blanco.damian@gmail.com>
2018-04-22 09:20:39 -0300

    Configuracion de la DB

---
+++ src/conexion/db/DB_Connection.java
@@ -0,0 +1,83 @@
+package conexion.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_Connection {
+	
+	/* --------------------------------------------------------------------------------------------- */
+	public Connection getConection(String url, String user, String password) {
+		String urlBase = "jdbc:mysql://localhost:3306/";
+		Connection myConect = null;
+		try { 
+			//			Class.forName("com.mysql.jdbc.Driver");
+			myConect = DriverManager.getConnection(urlBase+url, user, password);
+			if(myConect != null) {
+				System.out.println("Conexion establecida!");				
+			}
+		} catch (Exception e) {
+			//SQLException
+			e.printStackTrace();
+		} finally {
+			if(myConect == null) {
+				try {
+					myConect.close();
+					System.out.println("Conexion nula, cerrando!");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return myConect;
+	}
+
+	public void closeConnect(Connection c) {
+		try {
+			c.close();
+			if(c != null) {
+				System.out.println("Conexion cerrada!");				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/* --------------------------------------------------------------------------------------------- */
+	public Statement getStatement(Connection conex) {
+		Statement myStmt = null;
+		try {
+			myStmt = conex.createStatement();
+			if(myStmt != null) {
+				System.out.println("Statement logrado!");				
+			}
+		} catch (Exception e) {
+			//SQLException
+			e.printStackTrace();
+		}finally {
+			if(myStmt == null) {
+				try {
+					myStmt.close();
+					System.out.println("Statement cerrado!");		
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}		
+			}
+		}
+		return myStmt;
+	}
+
+	public void closeStatement(Statement s) {
+		try {
+			s.close();
+			if(s != null) {
+				System.out.println("Statement cerrada!");				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
