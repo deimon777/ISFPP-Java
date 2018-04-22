888eafc2e723308dbe6de2699104762cc20da539
Deimon <blanco.damian@gmail.com>
2018-04-22 09:21:02 -0300

    Contastes para la config de la app

---
+++ src/com/deimon/isfpp/configuracion/Constantes.java
@@ -0,0 +1,11 @@
+package com.deimon.isfpp.configuracion;
+
+public class Constantes {
+
+	/* Propieties */
+	public final static String DB_KEY  = "database";
+	public final static String DB_KEY_USER = "dbuser";
+	public final static String DB_KEY_PASS = "dbpassword";
+	/* GUI */
+	
+}
---
+++ src/com/deimon/isfpp/configuracion/ConstantesPropierties.java
@@ -0,0 +1,18 @@
+package com.deimon.isfpp.configuracion;
+
+public class ConstantesPropierties {
+
+	public static String DB_NAME_URL  = "a";
+	public static String DB_NAME_USER = "b";
+	public static String DB_NAME_PASS = "c";
+	
+	public void setDB_URL(String val) {
+		DB_NAME_URL = val;
+	}
+	public void setDB_USER(String val) {
+		DB_NAME_USER = val;
+	}
+	public void setDB_PASS(String val) {
+		DB_NAME_PASS = val;
+	}
+}
