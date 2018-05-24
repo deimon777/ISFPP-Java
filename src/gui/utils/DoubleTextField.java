d5f634ee1f9f125e5e8ffae4d7dd0dd474c28991
Deimon <blanco.damian@gmail.com>
2018-05-24 15:07:12 -0300

    Validador del Input double para latitud y longitud, y otros doubles que se necesiten

---
+++ src/gui/utils/DoubleTextField.java
@@ -0,0 +1,30 @@
+package gui.utils;
+
+import javafx.scene.control.TextField;
+
+public class DoubleTextField extends TextField{
+
+    @Override
+    public void replaceText(int start, int end, String text)
+    {
+        if (validate(text))
+        {
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String text)
+    {
+        if (validate(text))
+        {
+            super.replaceSelection(text);
+        }
+    }
+
+    private boolean validate(String text)
+    {
+        return text.matches("[-]?[0-9]*[\\.]{0,1}?[0-9]*"); //poner bien el double, q no permita --123...123.1.123
+        
+    }
+}
