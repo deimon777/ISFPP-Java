995d8c996dab8a79daf62b50a484039a2910dd67
deimon777 <blanco.damian@gmail.com>
2018-06-07 08:09:56 -0300

    Creadas las entidades para manipular los objetos

---
+++ src/com/deimon/entidades/ciudad/Alojamiento.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.deimon.entidades.ciudad;
+
+/**
+ * @author deimon
+ *
+ */
+public class Alojamiento {
+	private int id;
+	private String nombre;
+    private boolean activo;
+	private Ciudad ciudad_id = null;
+	
+	public Alojamiento() {		
+	}
+	
+	public Alojamiento(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/*
+	 * Getter y Setter
+	 */
+	public int getID() {
+		return id;
+	}
+	public void setID(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public boolean isActivo() {
+		return activo;
+	}
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	public Ciudad getCiudad_id() {
+		return ciudad_id;
+	}
+	public void setCiudad_id(Ciudad ciudad_id) {
+		this.ciudad_id = ciudad_id;
+	}	
+}
