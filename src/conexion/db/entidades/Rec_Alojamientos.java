dad714efed2b0cf6a7b69fd8249da7a65190ce88
deimon777 <blanco.damian@gmail.com>
2018-06-10 16:26:55 -0300

    Creada la consulta para traer un alojamiento, con la ciudad embebida

--- src/conexion/db/entidades/Rec_Alojamientos.java
+++ src/conexion/db/entidades/Rec_Alojamientos.java
@@ -2,8 +2,11 @@ package conexion.db.entidades;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.deimon.entidades.ciudad.Alojamiento;
+import com.deimon.entidades.ciudad.Ciudad;
 import com.deimon.isfpp.configuracion.ConstantesPropierties;
 
 import conexion.db.DB_Connection;
@@ -82,4 +85,61 @@ public class Rec_Alojamientos extends EntidadesUtils{
 	/*
 	 * BUSCAR
 	 */
+	
+	/**
+	 * Trae un alojamiento, con si ciudad
+	 * @param alojamientoId
+	 * @return Un alojamiento
+	 */
+	public Alojamiento getAlojamiento(int alojamientoId) {
+		String sql = "SELECT "
+				+ table_name+".*, "
+				+ "ciudades.nombre AS ciudad_nombre, "
+				+ "ciudades.habitantes, "
+				+ "ciudades.historia, "
+				+ "ciudades.latitud, "
+				+ "ciudades.longitud, "
+				+ "ciudades.activo AS ciudad_activa "
+				+ "FROM "+table_name+" JOIN `ciudades` "
+						+ "ON "+table_name+".ciudades_id=ciudades.id WHERE "+table_name+".id = ?";
+
+		DB_Connection conec = null;
+		Connection myConect = null;
+		PreparedStatement myPrepStmt = null;
+		ResultSet rs = null;
+		Alojamiento alojamiento = null;
+		try {
+			conec = new DB_Connection();
+			myConect = conec.getConection(ConstantesPropierties.DB_NAME_URL,
+					ConstantesPropierties.DB_NAME_USER,
+					ConstantesPropierties.DB_NAME_PASS);
+			if(myConect != null) {
+				myPrepStmt = myConect.prepareStatement(sql);
+				myPrepStmt.setInt(1, alojamientoId);
+				rs = myPrepStmt.executeQuery();				
+				while (rs.next()) {
+					alojamiento = new Alojamiento();
+					alojamiento.setID(rs.getInt("id"));
+					alojamiento.setNombre(rs.getString("nombre"));
+					alojamiento.setActivo(rs.getBoolean("activo"));
+					Ciudad c = new Ciudad();
+					c.setID(rs.getInt("ciudades_id"));
+					c.setNombre(rs.getString("ciudad_nombre"));
+					c.setHabitantes(rs.getInt("habitantes"));
+					c.setHistoria(rs.getString("historia"));
+					c.setLatitud(rs.getDouble("latitud"));
+					c.setLongitud(rs.getDouble("longitud"));
+					c.setActivo(rs.getBoolean("ciudad_activa"));
+					alojamiento.setCiudad(c);
+				}
+				rs.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			conec.closeConnect(myConect);
+		}
+		return alojamiento;
+	}
 }
