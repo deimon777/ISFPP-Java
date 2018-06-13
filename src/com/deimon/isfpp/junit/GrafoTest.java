0a100700a5da6fa0dc5af0e7512f07856e5726fd
deimon777 <blanco.damian@gmail.com>
2018-06-13 15:09:51 -0300

    Agregado para el Junit5

---
+++ src/com/deimon/isfpp/junit/GrafoTest.java
@@ -0,0 +1,14 @@
+package com.deimon.isfpp.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GrafoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
